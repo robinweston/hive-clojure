32e5f9430acc8fd58d4fb74a624fe8667e7ecc80
Robin Weston <lcfcforever@hotmail.com>
2015-01-20 22:42:10 +0000

    switched to using midje for tests

--- test/hive_clojure/grid_sizing_tests.clj
+++ test/hive_clojure/grid_sizing_tests.clj
@@ -1,11 +1,13 @@
 (ns hive-clojure.grid-sizing-tests
-  (:require [clojure.test :refer :all]
+  (:require [midje.sweet :refer :all]
             [hive-clojure.core :refer :all]))
 
 (defn parse-grid [raw-grid] [[0]])
 
-(deftest empty-grid-test
-  (testing "When parsing an empty grid it has the correct size"
-           (let [parsed-grid (parse-grid "")
-                 grid-width (count (first parsed-grid))]
-    (is (= grid-width 1)))))
+(facts "grid sizing"
+       (fact "Empty grid is sized correctly"
+             (let [parsed-grid (parse-grid "")
+                   grid-width (count (first parsed-grid))
+                   grid-length (count parsed-grid)]
+                  grid-width => 1
+                  grid-length => 1)))
