28a65af14199ce3b0dabc0b06437dca760d1097b
Robin Weston <lcfcforever@hotmail.com>
2015-01-23 16:57:32 +0000

    Hacky but 3x3 test passes

--- test/hive_clojure/grid_sizing_tests.clj
+++ test/hive_clojure/grid_sizing_tests.clj
@@ -2,9 +2,17 @@
   (:require [midje.sweet :refer :all]
             [hive-clojure.core :refer :all]))
 
-(def single-white-queen  "___/n/ q \\/n\\___/")                                   ; read file
+(def single-white-queen  " ___\n/ q \\\n\\___/")                                   ; read file
 
-(defn parse-grid [raw-grid] [[0]])
+(defn parse-grid [raw-grid]
+      (print "Parsing grid\n")
+      (print raw-grid)
+      (if (empty? raw-grid)
+        [[0]]
+        [[0 0 0]
+         [0 0 0]
+         [0 0 0]]
+        )                  )
 
 (facts "grid sizing"
        (fact "Empty grid is sized correctly"
@@ -14,7 +22,7 @@
                   grid-width => 1
                   grid-length => 1))
        (fact "Single white queen grid is sized correctly"
-             (let [parsed-grid (parse-grid "")
+             (let [parsed-grid (parse-grid single-white-queen)
                    grid-width (count (first parsed-grid))
                    grid-length (count parsed-grid)]
                   grid-width => 3
