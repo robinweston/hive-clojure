7c4c6fb8c575422f45d5c3d321fefffafa80bc4a
Robin Weston <lcfcforever@hotmail.com>
2015-01-23 16:46:01 +0000

    Added second failing test

--- test/hive_clojure/grid_sizing_tests.clj
+++ test/hive_clojure/grid_sizing_tests.clj
@@ -2,6 +2,8 @@
   (:require [midje.sweet :refer :all]
             [hive-clojure.core :refer :all]))
 
+(def single-white-queen  "___/n/ q \\/n\\___/")                                   ; read file
+
 (defn parse-grid [raw-grid] [[0]])
 
 (facts "grid sizing"
@@ -10,4 +12,10 @@
                    grid-width (count (first parsed-grid))
                    grid-length (count parsed-grid)]
                   grid-width => 1
-                  grid-length => 1)))
+                  grid-length => 1))
+       (fact "Single white queen grid is sized correctly"
+             (let [parsed-grid (parse-grid "")
+                   grid-width (count (first parsed-grid))
+                   grid-length (count parsed-grid)]
+                  grid-width => 3
+                  grid-length => 3)))
