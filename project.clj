32810e564de8c6df79be8fbf75863899f91166c3
Robin Weston <lcfcforever@hotmail.com>
2015-01-25 16:26:35 +0000

    Refactored tests slightly

--- project.clj
+++ project.clj
@@ -1,13 +1,15 @@
 (defproject hive-clojure "0.1.0-SNAPSHOT"
-  :description "Game engine for Hive"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main ^:skip-aot hive-clojure.core
-  :target-path "target/%s"
-  :profiles {
-             :uberjar {:aot :all}
-             :dev {
-                   :dependencies [[midje "1.6.0"]]
-                   :plugins [[lein-midje "3.1.3"]]
-                   }})
+            :description "Game engine for Hive"
+            :license {:name "Eclipse Public License"
+                      :url  "http://www.eclipse.org/legal/epl-v10.html"}
+            :dependencies [[org.clojure/clojure "1.6.0"]]
+            :main ^:skip-aot hive-clojure.core
+            :target-path "target/%s"
+            :profiles {
+                       :uberjar {:aot :all}
+                       :dev     {
+                                 :dependencies [
+                                                [midje "1.6.0"]
+                                                [local-file "0.1.0"]]
+                                 :plugins      [[lein-midje "3.1.3"]]
+                                 }})
