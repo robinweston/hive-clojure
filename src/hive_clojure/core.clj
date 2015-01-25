295c57098bdd38b2a33c8da789f894d51df31b77
Robin Weston <lcfcforever@hotmail.com>
2015-01-25 20:45:38 +0000

    Nearly working

--- src/hive_clojure/core.clj
+++ src/hive_clojure/core.clj
@@ -1,7 +1,26 @@
-(ns hive-clojure.core
-  (:gen-class))
+(ns hive-clojure.core)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn- calculate-grid-width [grid-lines]
+  (let [longest-grid-line (if (empty? grid-lines)
+                            0
+                            (max (map #(count %) grid-lines)))]
+    (if (= longest-grid-line 0)
+      1
+      (/ (+ longest-grid-line 7) 4))))
+
+(defn- calculate-grid-height [grid-lines]
+  (let [total-grid-lines (count grid-lines)]
+    (case total-grid-lines
+      0 1
+      3 3
+      (/ total-grid-lines 2))))
+
+(defn parse-grid-text [raw-grid]
+  (print "Parsing grid\n")
+  (print raw-grid)
+  (print "\n\n")
+
+  (let [grid-lines (clojure.string/split-lines raw-grid)
+        grid-width (calculate-grid-width grid-lines)
+        grid-height (calculate-grid-height grid-lines)]
+    (make-array Character/TYPE grid-height grid-width)))
--- test/hive_clojure/grid_sizing_tests.clj
+++ test/hive_clojure/grid_sizing_tests.clj
@@ -1,49 +1,29 @@
 (ns hive-clojure.grid-sizing-tests
-    (:require [midje.sweet :refer :all]
-      :require [hive-clojure.core :refer :all]
-      :require [local-file :refer :all]))
+  (:require [midje.sweet :refer :all])
+  (:require [local-file :refer :all])
+  (:require [hive-clojure.core :refer [parse-grid-text]]))
 
 (defn- slurp-grid-text-from-file [grid-file]
-       (slurp (clojure.java.io/file (project-dir) "test/grids/" (str grid-file ".txt"))))
+  (slurp (clojure.java.io/file (project-dir) "test/grids/" (str grid-file ".txt"))))
 
-(defn- assert-grid-dimensions [grid width height]
-       (let
-         [grid-width (count (first grid))
-          grid-length (count grid)]
-         grid-width => 1
-         grid-length => 1))
-
-(defn parse-grid-text [raw-grid]
-      (print "Parsing grid\n")
-      (print raw-grid)
-      (print "\n\n")
-
-      (if (empty? raw-grid)
-        [[0]]
-        [[0 0 0]
-         [0 0 0]
-         [0 0 0]]
-        ))
-
-(defn grid-dimensions [grid]
-      [(count (first grid))
-       (count grid)])
+(defn- retrieve-grid-dimensions [grid]
+  (vector (count (first grid)) (count grid)))
 
 (facts "grid sizing"
        (fact "Empty grid is sized correctly"
              (let [parsed-grid (parse-grid-text (slurp-grid-text-from-file "empty"))
-                   grid-dimensions (grid-dimensions parsed-grid)]
-                  (nth grid-dimensions 0) => 1
-                  (nth grid-dimensions 1) => 1))
+                   grid-dimensions (retrieve-grid-dimensions parsed-grid)]
+               (nth grid-dimensions 0) => 1
+               (nth grid-dimensions 1) => 1))
 
        (fact "Single white queen grid is sized correctly"
              (let [parsed-grid (parse-grid-text (slurp-grid-text-from-file "single-white-queen"))
-                   grid-dimensions (grid-dimensions parsed-grid)]
-                  (nth grid-dimensions 0) => 3
-                  (nth grid-dimensions 1) => 3))
+                   grid-dimensions (retrieve-grid-dimensions parsed-grid)]
+               (nth grid-dimensions 0) => 3
+               (nth grid-dimensions 1) => 3))
 
        (fact "Two horizontal queens grid is sized correctly"
              (let [parsed-grid (parse-grid-text (slurp-grid-text-from-file "two-queens-horizontal"))
-                   grid-dimensions (grid-dimensions parsed-grid)]
-                  (nth grid-dimensions 0) => 4
-                  (nth grid-dimensions 1) => 6)))
+                   grid-dimensions (retrieve-grid-dimensions parsed-grid)]
+               (nth grid-dimensions 0) => 4
+               (nth grid-dimensions 1) => 6)))
