29ef4fd8357044c8fb88ec8d98d122c1056ae70a
Robin Weston <lcfcforever@hotmail.com>
2015-01-25 22:09:28 +0000

    Remove empty strings after split

--- src/hive_clojure/core.clj
+++ src/hive_clojure/core.clj
@@ -3,7 +3,8 @@
 (defn- calculate-grid-width [grid-lines]
   (let [longest-grid-line (if (empty? grid-lines)
                             0
-                            (max (map #(count %) grid-lines)))]
+                            (max (map #(count %) grid-lines)))
+        _ (print "longest grid line" longest-grid-line)]
     (if (= longest-grid-line 0)
       1
       (/ (+ longest-grid-line 7) 4))))
@@ -15,12 +16,16 @@
       3 3
       (/ total-grid-lines 2))))
 
+(defn- create-empty-grid [width height]
+  (->> "-" (repeat width) vec (repeat height) vec))
+
 (defn parse-grid-text [raw-grid]
   (print "Parsing grid\n")
   (print raw-grid)
   (print "\n\n")
 
-  (let [grid-lines (clojure.string/split-lines raw-grid)
+  (let [split-lines (clojure.string/split-lines raw-grid)
+        grid-lines (filter #(not (clojure.string/blank? %)) split-lines)
         grid-width (calculate-grid-width grid-lines)
         grid-height (calculate-grid-height grid-lines)]
-    (make-array Character/TYPE grid-height grid-width)))
+    (create-empty-grid grid-width grid-height)))
