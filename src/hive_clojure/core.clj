18b86e06dab78ebe50f7c9a3ee96850e145381ae
Robin Weston <lcfcforever@hotmail.com>
2015-01-27 19:07:49 +0000

    Replaced loops with reduces and now tests all pass

--- src/hive_clojure/core.clj
+++ src/hive_clojure/core.clj
@@ -1,4 +1,5 @@
-(ns hive-clojure.core)
+(ns hive-clojure.core
+  (:require [hive-clojure.tile-parser :refer [parse-tile]]))
 
 (defn- longest-line-length [grid-lines]
   (if (empty? grid-lines)
@@ -18,16 +19,62 @@
       3 3
       (+ total-grid-lines 2))))
 
+(defn- split-grid-text-into-lines [grid-text]
+  (let [split-lines (clojure.string/split-lines grid-text)]
+    (filter #(not (clojure.string/blank? %)) split-lines)))
+
 (defn- create-empty-grid [width height]
   (->> :empty (repeat width) vec (repeat height) vec))
 
-(defn parse-grid-text [raw-grid]
+(defn- place-tile-on-grid [grid x y tile]
+  (let [line (grid y)                                       ; TODO: assoc in
+        new-line (assoc line x tile)]
+    (assoc grid y new-line)))
+
+(defn- re-pos [re s]
+  (loop [m (re-matcher re s)
+         res {}]
+    (if (.find m)
+      (recur m (assoc res (.start m) (.group m)))
+      res)))
+
+(defn- fill-grid-old [grid grid-lines]
+  (doseq [line-number (range (count grid-lines))]
+    (let [line (nth grid-lines line-number)
+          tile-matches (re-pos #"[A-Za-z]" line)]
+      (doseq [tile-match tile-matches]
+        (let [tile-x (-> (key tile-match) (- 2) (/ 4) inc)
+              tile-y (inc line-number)
+              tile (-> tile-match val parse-tile)]
+          (place-tile-on-grid grid tile-x tile-y tile)))))
+  grid)
+
+(defn- fill-grid-from-line-matches [grid line-number tile-matches]
+  (reduce (fn [new-grid tile-match]
+            (let [tile-x (-> (key tile-match) (- 2) (/ 4) inc)
+                  tile-y (inc line-number)
+                  tile (-> tile-match val parse-tile)]
+              (place-tile-on-grid new-grid tile-x tile-y tile))
+            )
+          grid tile-matches)
+  )
+
+(defn- fill-grid [grid grid-lines]
+  (reduce (fn [new-grid line-number]
+            (let [line (nth grid-lines line-number)
+                  tile-matches (re-pos #"[A-Za-z]" line)]
+              (fill-grid-from-line-matches new-grid line-number tile-matches)
+              )) grid (range (count grid-lines))))
+
+(defn parse-grid-text [grid-text]
   (print "Parsing grid\n")
-  (print raw-grid)
+  (print grid-text)
   (print "\n\n")
 
-  (let [split-lines (clojure.string/split-lines raw-grid)
-        grid-lines (filter #(not (clojure.string/blank? %)) split-lines)
+  (let [grid-lines (split-grid-text-into-lines grid-text)
         grid-width (calculate-grid-width grid-lines)
-        grid-height (calculate-grid-height grid-lines)]
-    (create-empty-grid grid-width grid-height)))
+        grid-height (calculate-grid-height grid-lines)
+        empty-grid (create-empty-grid grid-width grid-height)
+        filled-grid (fill-grid empty-grid grid-lines)]
+    filled-grid
+    ))
--- test/hive_clojure/tile_positioning_tests.clj
+++ test/hive_clojure/tile_positioning_tests.clj
@@ -2,8 +2,15 @@
   (:require [midje.sweet :refer :all])
   (:require [hive-clojure.helpers.test-grid-parser :refer :all]))
 
+(defn cell-at [grid x y]
+         ((grid y) x))
+
 (facts "Tiles are positioned correctly when parsing grid"
        (fact "Empty grid has no tiles"
              (let [grid (parse-test-grid "empty")]
-               (first (first grid)) => :empty
-               )))
+               (cell-at grid 0 0) => :empty))
+
+       (fact "Single white queen is in correct position"
+             (let [grid (parse-test-grid "single-white-queen")]
+               (:color (cell-at grid 1 2)) => :white
+               (:insect (cell-at grid 1 2)) => :queen))) ; TODO: assert all others empty
