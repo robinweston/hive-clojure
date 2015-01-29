dd795950a15838f664c8e00a1bd89a5d1c7aab99
Robin Weston <lcfcforever@hotmail.com>
2015-01-29 21:58:42 +0000

    Added tests for turn number

---
+++ test/hive_clojure/turn_ordering_tests.clj
@@ -0,0 +1,24 @@
+(ns hive-clojure.turn-ordering-tests
+  (:require [midje.sweet :refer :all]))
+
+(defn next-to-play [game-state]
+  (if (= 0 (mod (:turn-number game-state) 2))
+    :white
+    :black
+    ))
+
+(facts "about turn ordering"
+       (fact "White plays first"
+             (let [game-state {:turn-number 0}]
+               (next-to-play game-state) => :white
+               ))
+
+       (fact "Black plays second"
+             (let [game-state {:turn-number 1}]
+               (next-to-play game-state) => :black
+               ))
+
+       (fact "White plays thirds"
+             (let [game-state {:turn-number 2}]
+               (next-to-play game-state) => :white
+               )))
